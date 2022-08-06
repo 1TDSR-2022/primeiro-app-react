0847a0ac25a7f03dddef86c5bdd4d7b91bc051b9
odavisilva <odavisilva>
2022-08-05 23:04:20 -0300

    Iniciando React

---
+++ rm95535/aula01/src/components/Primeiro/Primeiro.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './Primeiro.css'
+export default function Primeiro() {
+    return (
+        <div>
+             <h3>Primeiro componente</h3>
+        </div>
+    )
+}
