69ffae5e8942ac8f529dcfa73f855f3ff85f458a
odavisilva <rm95535@fiap.com.br>
2022-08-10 22:49:29 -0300

    Add Lista de Alunos

--- rm95535/aula01/src/App.jsx
+++ rm95535/aula01/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import ListaDeAlunos from './components/ListaDeAlunos/ListaDeAlunos'
 import Primeiro from './components/Primeiro/Primeiro'
 
 export default function App() {
-    const tag = <h1><strong>Olá React!</strong></h1>
+    const aluno = "Valentina Fuzaro"
+    const turma = "1TDSR"
+    const profs = ["Josef", "Alexandre", "Salvio", "Eduardo", "Miguel", "Cordeiro"]
+
     return (
-        <div>
-            <h1>App-React</h1>
-        { tag }
-            <h2>Subtitulo</h2>
-        </div>
+        <>
+            <h2>Nome do aluno:{aluno}</h2>
+            <ListaDeAlunos
+            nomeProps={aluno}
+            turmaProps={turma}
+            profsProps={profs} 
+            />
+        </>
     )
 }
