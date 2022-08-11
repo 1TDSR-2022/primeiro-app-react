9386f313ae60ce7c63817e369f61ef7fe5daeb40
lucasmoraisnt <RM93267@fiap.com.br>
2022-08-11 20:43:11 -0300

    Primeira aplicação com componentes funcionais

---
+++ rm93267/aula01/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Primeiro from './components/Primeiro/Primeiro'
+import Segundo from './components/Segundo/Segundo'
+
+    export default function App() {
+
+        const aluno = "Lucas Morais"
+        const turma = "1TDSR"
+        const profs = ["Alexandre","Josefe","Sálvio","Eduardo","Miguel","Cordeiro"]
+
+
+    return (
+        <>
+          <h2>Nome do aluno: {aluno}</h2>
+          
+        </>
+    )
+}
