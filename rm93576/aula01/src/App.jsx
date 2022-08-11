24c1ecd95e2a6916181b6383796e09aa4f6cdb8a
abner-ferreira <rm93576@fiap.com.br>
2022-08-10 22:47:00 -0300

    Inicializando Props

--- rm93576/aula01/src/App.jsx
+++ rm93576/aula01/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import ListaDeAlunos from './components/ListaDeAlunos/ListaDeAlunos';
 import Primeiro from './components/Primeiro/Primeiro';
 import Segundo from './components/Segundo/Segundo';
 
 export default function App(){
-    const tag = <h1><strong>Olá React</strong></h1>
+
+    const aluno = "Valentina Fuzaro"
+    const turma = "1TDSR"
+    const profs = ["Josef", "Alexandre", "Sálvio", "Eduardo", "Miguel", "Cordeiro"]
+
     return(
-        <>
-            <h1>App-React</h1>
-            {tag}
-            <h2>Subtítulo</h2>
-            <Primeiro />
-            <Segundo />
+        < >
+           <h2>Nome do aluno: {aluno}</h2>
+           <ListaDeAlunos 
+           nomeProps={aluno}
+           turmaProps ={turma}
+           profsProps={profs}
+           />
         </>
     )
 }
