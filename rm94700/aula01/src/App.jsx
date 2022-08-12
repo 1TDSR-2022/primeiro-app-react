b462cd1315a574d94e8a002f208c65b0395a695d
K.Altino <kauanaltino@gmail.com>
2022-08-11 21:01:17 -0300

    Exercicio de props

--- rm94700/aula01/src/App.jsx
+++ rm94700/aula01/src/App.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
-import ListaDeAlunos from './components/ListaDeAlunos/ListaDeAlunos'
-import Primeiro from './components/Primeiro/Primeiro'
-import Segundo from './components/Segundo/Segundo'
+//import ListaDeAlunos from './components/ListaDeAlunos/ListaDeAlunos'
+import ModelosDeCarros from './components/ModelosDeCarros/ModelosDeCarros'
+import carroImg from './img/charger1970.webp'
+//import Primeiro from './components/Primeiro/Primeiro'
+//import Segundo from './components/Segundo/Segundo'
 
 export default function App() {
 
-    const aluno = "Valentina Fuzaro"
-    const turma = "1TDSR"
-    const profs = ["Josef", "Alexandre", "Sálvio", "Eduardo", "Miguel"]
+    // const aluno = "Valentina Fuzaro"
+    // const turma = "1TDSR"
+    // const profs = ["Josef", "Alexandre", "Sálvio", "Eduardo", "Miguel"]
+    
+   
+
+    const infoCarro = "Carro de Familia"
+    const infoImg = 200
+    const leg = "Esse carro fez história"
+    const listaDeCarros = ["Dodge Charger", "Golf-GTI", "GOL-G5", "Dodge Challenger"]
 
 return(
-    <React.Fragment>
-        <h2>Nome do Aluno: {aluno}</h2>
+    <div>
+        {/* <h2>Nome do Aluno: {aluno}</h2>
         <ListaDeAlunos 
         nomeProps={aluno}
         turmaProps={turma}
         profsProps={profs}
+        /> */}
 
-        />
-    </React.Fragment>
+        <ModelosDeCarros>
+            pathImgProps={carroImg}
+            infoCarroProps={infoCarro}
+            infoImgProps={infoImg}
+            legProps={leg}
+            listaDeCarrosProps={listaDeCarros}
+        </ModelosDeCarros>
+    </div>
 )
 }
+
+
--- rm94700/aula01/src/index.js
+++ rm94700/aula01/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-//Importando um arquivo CSS
+//Importando um arquivo css
 import './index.css'
+
 
 ReactDOM.render(<App/>,document.getElementById('root'))
