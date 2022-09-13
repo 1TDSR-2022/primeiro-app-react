fcbbe79675cce44f28bb48d99d0c2f12c1110c3d
LuisHPerestrelo <rm93738@fiap.com.br>
2022-09-13 19:16:44 -0300

    Lista-Props

--- rm93738/aula01/src/App.jsx
+++ rm93738/aula01/src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import Primeiro from './components/Primeiro/Primeiro'
 import Segundo from './components/Segundo/Segundo'
+import ListaDeAlunos from './components/ListaDeAlunos/ListaDeAluno'
 import './index.css'
 
-
-
 export default function App(){
-    const tag = <h1>App React</h1>
+    
+    const aluno = "Luis Perestrelo"
+    const turma = "1TDSR"
+    const profs = ["Joseff", "Alexandre", "Sálvio", "Eduardo", "Cordeiro", "Miguel"]
+
     return(
-        <div>
-        <h1>App-React</h1>
-        <h2>Subtitulo</h2>
-        {tag}
-        <Primeiro/>
-        <Segundo/>
-        </div>
+        <>
+            <h2>Nome do aluno:{aluno}</h2>
+            <ListaDeAlunos
+            nomeProps={aluno}
+            turmaProps={turma}
+            profsProps={profs}
+            />
+            
+        </>
     )
 } 
